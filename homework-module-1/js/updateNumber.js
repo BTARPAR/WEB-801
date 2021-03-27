ea77bf9204e836d29079c6fc20bdb99a279fe0cf
Bhargav Tarpara <tarpara_bhargav@yahoo.in>
2021-03-26 19:27:05 -0700

    Git init

--- homework-module-1/js/updateNumber.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-  $('textarea#exampleFormControlTextarea1').on('input', (event) => {
-    $('#updateBadge').text($('textarea#exampleFormControlTextarea1').val().length)
-  })
-})
